55601977273bfc3b907d1fe349f67153f03c50fd
Camilo Cangaro <camilocangaro@gmail.com>
2024-05-20 09:01:35 -0300

    Update page title in welcome.blade.php - Prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Laravel React</title>
 
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
