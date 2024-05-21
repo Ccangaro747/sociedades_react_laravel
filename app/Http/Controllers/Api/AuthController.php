eda98eca66af55bfe7eb9062f3f2b9f36df5e757
Camilo Cangaro <camilocangaro@gmail.com>
2024-05-21 12:19:03 -0300

    Implementando registro e inicio de sesión de usuarios en la API

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,10 +2,65 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request) // Este método se utiliza para registrar un nuevo usuario en la aplicación. Recibe los datos del formulario de registro y crea una nueva cuenta de usuario en la base de datos. Request es una clase que representa la solicitud HTTP y se utiliza para acceder a los datos enviados por el usuario.
+    {
+        // Inicializa la respuesta con un indicador de éxito establecido en "false". Esto se utilizará para indicar si el registro fue exitoso o no.
+        $response = ["success" => false];
+        //Validacion
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $response = ["error" => $validator->errors()];
+            return response()->json($response, 200);
+        }
+        // Recoge todos los datos del formulario de la solicitud.
+        $input = $request->all();
+        // Encripta la contraseña proporcionada por el usuario.
+        $input["password"] = bcrypt($input['password']);
+        // Crea un nuevo usuario en la base de datos con los datos proporcionados. Bcrypt es una función de hash de contraseñas que se utiliza para almacenar contraseñas de forma segura en la base de datos.
+        $user = User::create($input);
+        // Asigna el rol 'admin' al usuario recién creado. assignRole es un método proporcionado por el paquete de permisos de Spatie que se utiliza para asignar un rol a un usuario.
+        $user->assignRole('client');
+        // Establece el indicador de éxito en "true" ya que el usuario se ha creado correctamente.
+        $response["success"] = true;
+        // Genera un token de autenticación para el usuario recién registrado. plainTextToken es un método proporcionado por Sanctum que se utiliza para generar un token de autenticación para el usuario.
+        //$response["token"] = $user->createToken("myApp")->plainTextToken;
+        // Devuelve una respuesta JSON con el indicador de éxito y el token de autenticación.
+        return response()->json($response, 200);
+    }
+
+    public function login(Request $request)
+    {
+        // Inicializa la respuesta con un indicador de éxito establecido en "false". Esto se utilizará para indicar si el registro fue exitoso o no.
+        $response = ["success" => false];
+        //Validacion
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $response = ["error" => $validator->errors()];
+            return response()->json($response, 200);
+        }
+        if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = auth()->user();
+            $user->hasRole('client'); //Verifica si el usuario tiene el rol de cliente
+
+            $response['token'] = $user->createToken('myApp')->plainTextToken;
+            $response['user'] = $user;
+            $response['success'] = true;
+        }
+        return response()->json($response, 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,56 +9,66 @@ use App\Http\Controllers\Api\Client\EntidadController as EmpresaClient;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function(){
+Route::prefix('v1')->group(function () {
 
-//PUBLIC<--
+    //PUBLIC<--
 
-//::public
+    //::public
 
-//Las rutas publicas son accesibles para cualquier usuario, no requieren autenticación.
+    //Las rutas publicas son accesibles para cualquier usuario, no requieren autenticación.
 
-Route::get('/public/{slug}', [FrontController::class,'categoria']); // Esta ruta se utiliza para mostrar una categoría específica en la parte pública de la aplicación. El parámetro 'slug' se utiliza para identificar de manera única la categoría que se va a mostrar. El controlador 'FrontController' y su método 'categoria' se encargan de procesar la solicitud.
-
-//::auth
-
-//Tambien es una ruta publica los formularios de registro y login.
-
-Route::get('/auth/register', [AuthController::class,'register']); // Esta ruta se utiliza para mostrar el formulario de registro en la parte pública de la aplicación. El controlador 'AuthController' y su método 'register' se encargan de procesar la solicitud.
-Route::get('/auth/login', [AuthController::class,'login']); // Esta ruta se utiliza para mostrar el formulario de inicio de sesión en la parte pública de la aplicación. El controlador 'AuthController' y su método 'login' se encargan de procesar la solicitud.
-
-//PRIVATE<--
-
-//::privada
-
-//Las rutas privadas son accesibles solo para usuarios autenticados, requieren autenticación.
-
-//Utilizamos el middleware 'auth:sanctum' para proteger las rutas privadas. Este middleware verifica si el usuario está autenticado antes de permitir el acceso a la ruta.
-
-Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::get('/public/{slug}', [FrontController::class, 'categoria']); // Esta ruta se utiliza para mostrar una categoría específica en la parte pública de la aplicación. El parámetro 'slug' se utiliza para identificar de manera única la categoría que se va a mostrar. El controlador 'FrontController' y su método 'categoria' se encargan de procesar la solicitud.
 
     //::auth
 
-    //Logout por que es una ruta privada y se necesita autenticación.
+    //Tambien es una ruta publica los formularios de registro y login.
 
-    Route::post('/auth/logout', [AuthController::class,'logout']);
+    Route::post('/auth/register', [AuthController::class, 'register']); // Esta ruta se utiliza para mostrar el formulario de registro en la parte pública de la aplicación. El controlador 'AuthController' y su método 'register' se encargan de procesar la solicitud.
+    Route::post('/auth/login', [AuthController::class, 'login']); // Esta ruta se utiliza para mostrar el formulario de inicio de sesión en la parte pública de la aplicación. El controlador 'AuthController' y su método 'login' se encargan de procesar la solicitud.
+    /*
 
-    //::ruta del rol cliente
+    Las rutas de registro y login se cambiaron del GET a POST porque estas rutas están diseñadas para recibir datos del usuario (como el correo electrónico y la contraseña) y luego realizar una acción con esos datos (crear una cuenta o iniciar sesión).
 
-    //Client por que es una ruta privada y se necesita autenticación.
+    En el caso del registro, los datos del formulario de registro se envían al servidor, donde se crea una nueva cuenta de usuario. En el caso del inicio de sesión, los datos del formulario de inicio de sesión se envían al servidor, donde se verifica la identidad del usuario.
 
-    Route::apiResource('/client/entidad', EntidadController::class); //apiResource es un método que crea automáticamente las rutas para un recurso RESTful. En este caso, estamos creando las rutas para el recurso 'entidad' del controlador 'EntidadController'. Estas rutas permiten realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) sobre las entidades de la aplicación.
+    En contraste, el método GET se utiliza para solicitar datos del servidor. No se debería usar GET para el registro o el inicio de sesión porque estos procesos implican el envío de datos sensibles (como contraseñas) al servidor, y GET no es seguro para este propósito porque los datos enviados con GET se añaden a la URL y pueden ser vistos en el historial del navegador, los registros del servidor, etc.
 
-    //::ruta del rol admin
+    Por lo tanto, es una práctica común y recomendada utilizar POST para las rutas de registro y login.
 
-    //Admin por que es una ruta privada y se necesita autenticación.
+    */
 
-    Route::apiResource('/admin/user', UserController::class);
-    Route::apiResource('/admin/categoria', CategoriaController::class);
-    Route::apiResource('/admin/entidad', EmpresaClient::class);
+    //PRIVATE<--
 
-});
+    //::privada
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    //Las rutas privadas son accesibles solo para usuarios autenticados, requieren autenticación.
+
+    //Utilizamos el middleware 'auth:sanctum' para proteger las rutas privadas. Este middleware verifica si el usuario está autenticado antes de permitir el acceso a la ruta.
+
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+
+        //::auth
+
+        //Logout por que es una ruta privada y se necesita autenticación.
+
+        Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+        //::ruta del rol cliente
+
+        //Client por que es una ruta privada y se necesita autenticación.
+
+        Route::apiResource('/client/entidad', EntidadController::class); //apiResource es un método que crea automáticamente las rutas para un recurso RESTful. En este caso, estamos creando las rutas para el recurso 'entidad' del controlador 'EntidadController'. Estas rutas permiten realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) sobre las entidades de la aplicación.
+
+        //::ruta del rol admin
+
+        //Admin por que es una ruta privada y se necesita autenticación.
+
+        Route::apiResource('/admin/user', UserController::class);
+        Route::apiResource('/admin/categoria', CategoriaController::class);
+        Route::apiResource('/admin/entidad', EmpresaClient::class);
+    });
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
