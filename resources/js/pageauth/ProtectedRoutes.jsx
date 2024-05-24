441334746e2f9432e8ac5f28944be6f7dc5b1c98
Camilo Cangaro <camilocangaro@gmail.com>
2024-05-24 13:30:29 -0300

    Implementar el Registro de usuarios

--- resources/js/pageauth/ProtectedRoutes.jsx
+++ resources/js/pageauth/ProtectedRoutes.jsx
@@ -3,11 +3,13 @@ import { Navigate, Outlet } from "react-router-dom";
 import AuthUser from "./AuthUser";
 
 const ProtectedRoutes = () => {
-    const { getToken } = AuthUser()
-    if(!getToken()){
-        return <Navigate to={"/login"}/>;
+    // Obtenemos la función getToken del objeto que devuelve AuthUser
+    const { getToken } = AuthUser();
+    // Si el token no existe (es decir, el usuario no está autenticado), redirigimos a la página de inicio de sesión
+    if (!getToken()) {
+        return <Navigate to={"/login"} />;
     }
-
+    // Si el token existe (es decir, el usuario está autenticado), renderizamos los componentes de las rutas anidadas
     return <Outlet />;
 };
 
