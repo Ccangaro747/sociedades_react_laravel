f8165b3d12a05e493b8272114436218f2b0cc1f9
Camilo Cangaro <camilocangaro@gmail.com>
2024-05-24 13:03:43 -0300

    Agregando comentarios AuthUser.jsx

--- resources/js/pageauth/AuthUser.jsx
+++ resources/js/pageauth/AuthUser.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AuthUser = () => {
-    const navigate = useNavigate();
+    const navigate = useNavigate(); // Importamos el hook useNavigate para redireccionar a diferentes rutas
 
+    // Funciones para obtener el token, usuario y rol del sessionStorage
     const getToken = () => {
         const tokenString = sessionStorage.getItem("token");
         const token = JSON.parse(tokenString);
@@ -20,11 +21,11 @@ const AuthUser = () => {
         const rol = JSON.parse(rolString);
         return rol;
     };
-
+    // Estados locales para token, usuario y rol, inicializados con los valores del sessionStorage
     const [token, setToken] = useState(getToken());
     const [user, setUser] = useState(getUser());
     const [rol, setRol] = useState(getRol());
-
+    // Función para guardar el token, usuario y rol en sessionStorage y actualizar los estados locales
     const saveToken = (user, token, rol) => {
         sessionStorage.setItem("user", JSON.stringify(user));
         sessionStorage.setItem("token", JSON.stringify(token));
@@ -36,24 +37,25 @@ const AuthUser = () => {
 
         // rol : admin | client
 
+        // Redirección a las rutas correspondientes según el rol del usuario
         if (getRol() === "admin") navigate("/admin");
         if (getRol() === "client") navigate("/client");
     };
-
+    // Función para cerrar sesión, limpiando el sessionStorage y redirigiendo a la página de inicio
     const getLogout = () => {
         sessionStorage.clear();
         navigate("/");
     };
-
+    // Retornamos un objeto con las funciones y valores relevantes para ser utilizados por otros componentes
     return {
-        setToken: saveToken,
-        token,
-        user,
-        rol,
-        getToken,
-        getRol,
-        getUser,
-        getLogout,
+        setToken: saveToken, // Función para establecer el token
+        token, // Valor del token
+        user, // Valor del usuario
+        rol, // Valor del rol
+        getToken, // Función para obtener el token
+        getRol, // Función para obtener el rol
+        getUser, // Función para obtener el usuario
+        getLogout, // Función para cerrar sesión
     };
 };
 
