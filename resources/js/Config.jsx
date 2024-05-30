59abb3d2f4d037223a465d3565c2ac55a06622fc
Camilo Cangaro <camilocangaro@gmail.com>
2024-05-30 13:05:40 -0300

    Implementación completa del Logout de usuarios

--- resources/js/Config.jsx
+++ resources/js/Config.jsx
@@ -8,5 +8,5 @@ export default{
     //Auth
     getRegister:(data)=>axios.post(`${base_api_url}/auth/register`,data),
     getLogin:(data)=>axios.post(`${base_api_url}/auth/login`,data),
-    getLogout:(data)=>axios.post(`${base_api_url}/auth/logout`,data)
+    getLogout:(data)=>axios.post(`${base_api_url}/auth/logout`,data) //Se crea en el Navbar.jsx, se podría eliminar
 }
--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,36 +1,51 @@
 import React from "react";
+import axios from 'axios';  // Importar axios
 import AuthUser from "../pageauth/AuthUser";
-import Config from '../Config'
 
 const Navbar = () => {
-    const {getRol, getLogout, getToken } = AuthUser();
+    const { getRol, getLogout, getToken } = AuthUser();
 
+    // Definimos una función llamada 'logoutUser'
     const logoutUser = () => {
-        const token = getToken(); // Obtén el token de autenticación
 
+        const token = getToken(); // Obtenemos el token de autenticación del usuario actual llamando a la función 'getToken'
+
+        // Hacemos una solicitud POST a la ruta '/api/v1/auth/logout' del servidor
         axios.post("/api/v1/auth/logout", {}, {
+            // Incluimos el token de autenticación en las cabeceras de la solicitud
             headers: {
                 'Authorization': `Bearer ${token}` // Envía el token en la cabecera de la solicitud
             }
+        // Si la solicitud es exitosa, el servidor responderá con una respuesta
         }).then((response) => {
-            console.log(response)
+            console.log(response);
+            // Llamamos a la función 'getLogout' que probablemente se encarga de actualizar el estado de la aplicación
+            // para reflejar que el usuario ha cerrado la sesión
             getLogout();
         }).catch((error) => {
-            console.log(error);
+            console.error('Error al cerrar sesión:', error);
         });
     };
 
+    // Esta función se encarga de renderizar los enlaces de la barra de navegación
     const renderLinks = () => {
+        // Comprueba si el usuario está autenticado (si hay un token)
         if (getToken()) {
+            // Si el usuario está autenticado, se devuelven dos enlaces: Administración y Logout
             return [
+                // Enlace a la página de administración. El destino de este enlace se determina
+                // por el rol del usuario autenticado (obtenido con getRol())
+                // Se utiliza key en react para identificar de forma única a cada elemento de la lista
                 <li className="mr-3" key="admin">
                     <a
                         className="inline-block px-4 py-2 text-gray-600 no-underline hover:text-gray-200 hover:text-underline"
                         href={`/${getRol()}`}
                     >
-                        Administracion
+                        Administración
                     </a>
                 </li>,
+                // Enlace para cerrar la sesión del usuario. Al hacer clic en este enlace,
+                // se llama a la función logoutUser
                 <li className="mr-3" key="logout">
                     <a
                         className="inline-block px-4 py-2 text-gray-600 no-underline hover:text-gray-200 hover:text-underline"
@@ -42,6 +57,7 @@ const Navbar = () => {
                 </li>
             ];
         } else {
+                // Si el usuario no está autenticado, se devuelve un solo enlace a la página de inicio de sesión
             return [
                 <li className="mr-3" key="login">
                     <a
@@ -63,7 +79,7 @@ const Navbar = () => {
                         className="text-white no-underline hover:text-black hover:no-underline"
                         href="#"
                     >
-                        <img src="/logo.png" alt="Logo" className="object-contain h-20 w-30" />
+                        <img src="/logo.png" alt="Logo" className="object-contain h-20 w-30"/>
                     </a>
                 </div>
                 <div className="flex content-center justify-between w-full pt-2 md:w-1/2 md:justify-end">
@@ -81,7 +97,7 @@ const Navbar = () => {
                                 className="inline-block px-4 py-2 text-gray-600 no-underline hover:text-gray-200 hover:text-underline"
                                 href="/categorias"
                             >
-                                Categorias
+                                Categorías
                             </a>
                         </li>
                         {renderLinks()}
