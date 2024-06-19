0a286e02e1993a0eb4ea8bfcf5a4432de2c156eb
Camilo Cangaro <camilocangaro@gmail.com>
2024-06-19 11:41:29 -0300

    Implementando método show y update en EntidadController de client

--- app/Http/Controllers/Api/Client/EntidadController.php
+++ app/Http/Controllers/Api/Client/EntidadController.php
@@ -34,4 +34,31 @@ class EntidadController extends Controller
         $data->save();
         return response()->json($data, 200);
     }
+    public function show($id)
+    {
+        $data = Entidad::find($id);
+        return response()->json($data, 200);
+    }
+    public function update(Request $request, $id)
+    {
+        //Acá se puede realizar una validación de los datos que llegan en la petición si es necesario...
+        //Ej; $request->validate(['nombre' => 'required']);
+        $data = Entidad::find($id);
+        $data->fill($request->all());
+        //Upload image base64
+        if ($request->urlfoto) {
+            $img = $request->urlfoto;
+            //Process image
+            $folderPath = "/img/categoria/";
+            $image_parts = explode(";base64,", $img);
+            $image_type_aux = explode("image/", $image_parts[0]);
+            $image_type = $image_type_aux[1];
+            $image_base64 = base64_decode($image_parts[1]);
+            $file = $folderPath . Str::slug($request->nombre) . '.' . $image_type;
+            file_put_contents(public_path($file), $image_base64);
+            $data->urlfoto  =   Str::slug($request->nombre) . '.' . $image_type;
+        }
+        $data->save();
+        return response()->json($data, 200);
+    }
 }
