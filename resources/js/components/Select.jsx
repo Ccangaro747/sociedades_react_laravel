cfb2d1d5d4eaacbe67a2cf660bebc88a3cc78ef0
Camilo Cangaro <camilocangaro@gmail.com>
2024-06-26 11:13:11 -0300

    Modificado Select.jsx para obtener opciones desde la API utilizando el token obtenido a través de AuthUser. Este cambio mejora la seguridad y autonomía del componente al manejar la obtención de datos de manera más segura y eficiente.

--- resources/js/components/Select.jsx
+++ resources/js/components/Select.jsx
@@ -1,25 +1,39 @@
 import React, { useEffect, useState } from "react";
-import Config from "../Config";
+import axios from "axios";
+import AuthUser from "../pageauth/AuthUser";
 
 const Select = ({ data, selected }) => {
     const [options, setOptions] = useState([]);
+    const { getToken } = AuthUser();
+
     useEffect(() => {
         getOptions();
     }, []);
 
     const getOptions = async () => {
-        const response = await Config.getCategoriaAll();
-        setOptions(response.data);
+        const token = getToken(); 
+        try {
+            const response = await axios.get(
+                "http://localhost:8000/api/v1/admin/categoria",
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                },
+            );
+            setOptions(response.data);
+        } catch (error) {
+            console.error("Error al obtener las opciones:", error);
+        }
     };
+
     return (
-        <select className='form-control' value={data} onChange={(e)=>{selected(e.target.value)}}>
-            {options.map((option) => {
-                return (
-                    <option key={option.id} value={option.id}>
-                        {option.nombre}
-                    </option>
-                );
-            })}
+        <select className='form-control' value={data} onChange={(e) => selected(e.target.value)}>
+            {options.map((option) => (
+                <option key={option.id} value={option.id}>
+                    {option.nombre}
+                </option>
+            ))}
             <option value=""></option>
         </select>
     );
