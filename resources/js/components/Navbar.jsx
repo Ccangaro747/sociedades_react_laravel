57609f7851c45f338afffb29a0e55e397009574d
Camilo Cangaro <camilocangaro@gmail.com>
2024-07-08 13:57:51 -0300

    Se agregó menú hamburguesa para la vista móvil en el componente Navbar

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';  // Importar axios
+import axios from 'axios';
 import AuthUser from "../pageauth/AuthUser";
 
 const Navbar = () => {
     const { getRol, getLogout, getToken, user } = AuthUser();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const logoutUser = async () => {
         const token = getToken();
@@ -61,18 +62,26 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="w-full p-2 mt-0 bg-white shadow-md">
-            <div className="container flex flex-wrap items-center mx-auto">
-                <div className="flex justify-center w-full font-extrabold text-white md:w-1/2 md:justify-start">
+        <nav className="w-full p-4 mt-0 bg-white shadow-md">
+            <div className="container flex flex-wrap items-center justify-between mx-auto">
+                <div className="flex items-center justify-between w-full md:w-auto">
                     <a
                         className="text-white no-underline hover:text-black hover:no-underline"
                         href="#"
                     >
                         <img src="/logo.png" alt="Logo" className="object-contain h-20 w-30" />
                     </a>
+                    <button
+                        className="text-gray-600 md:hidden"
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    >
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                        </svg>
+                    </button>
                 </div>
-                <div className="flex content-center justify-between w-full pt-2 md:w-1/2 md:justify-end">
-                    <ul className="flex flex-col items-center justify-between flex-1 list-reset md:flex-none md:flex-row">
+                <div className={`w-full md:flex md:items-center md:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
+                    <ul className="flex flex-col items-center justify-center flex-1 list-reset md:flex-row md:items-center">
                         <li className="mr-3">
                             <a
                                 className="inline-block px-4 py-2 text-gray-600 no-underline hover:text-gray-200 hover:text-underline"
