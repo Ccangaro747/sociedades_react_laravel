4d724aa0d64f7615e36587f5e247b11a8c525102
Camilo Cangaro <camilocangaro@gmail.com>
2024-07-10 09:42:29 -0300

    feat: Agregar footer con derechos de autor en la página
    Se agregó un footer con los derechos de autor en la página para cumplir con los requisitos legales y brindar información sobre la propiedad de los contenidos.
    
    This commit adds a footer with copyright information to the page to comply with legal requirements and provide information about content ownership.

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer className="w-full p-4 bg-white shadow-md">
+            <div className="container flex flex-wrap items-center justify-between mx-auto">
+                <div className="flex items-center justify-center w-full md:w-auto">
+                    <a
+                        className="text-gray-600 no-underline hover:text-black hover:no-underline"
+                        href="#"
+                    >
+                        © 2024 Municipalidad de Mercedes. Todos los derechos
+                        reservados.
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
