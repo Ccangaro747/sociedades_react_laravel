b13a1990df3293f99eed41b735975d8d52193e3c
Camilo Cangaro <camilocangaro@gmail.com>
2024-07-15 12:25:26 -0300

    Ajustes logo y título

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Laravel React</title>
+    <title>Municipalidad de Mercedes</title>
+
+    <link rel="icon" href="{{ asset('logomuni.png') }}" type="image/x-icon">
 
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
